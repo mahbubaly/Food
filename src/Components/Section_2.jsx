e365a5f88f2dfecb17d9bca3b609d660ece71545
Mahbubaly <mahbubalikhanofficial@gmail.com>
2023-05-06 00:54:55 +0600

    laptop scroll in the windo w

--- src/Components/Section_2.jsx
+++ src/Components/Section_2.jsx
@@ -35,12 +35,12 @@ const Section_2 = () => {
                 <div className='lg:-mt-[300px]  xl:ml-80  lg:ml-48 lg:p-0 p-5  justify-center lg:flex items-center'>
 
 
-                    <div className='awards  xl:p-11 lg:p-4 p-5 mt-4 lg:mt-10  '>
+                    <div data-aos="fade-down"  data-aos-offset="300" data-aos-easing="ease-in-sine" data-aos-duration="600"  className='awards  xl:p-11 lg:p-4 p-5 mt-4 lg:mt-10  '>
                         <h1 className='font-bold lg:text-[36px] text-[16px]  xl:mt-0 lg:text-center text-left '>200+ International Awards</h1>
                         <p >Wasier to reach your savings goals when you have the right savings.</p>
                     </div>
 
-                    <img className='lg:mt-0 mt-6' src={img2} alt="" />
+                    <img data-aos="fade-up"  data-aos-offset="300" data-aos-easing="ease-in-sine" data-aos-duration="600" className='lg:mt-0 mt-6' src={img2} alt="" />
                 </div>
             </div>
 
